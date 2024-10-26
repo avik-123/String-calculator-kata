620624266ba6f1baaa547b7a112688164dc759b0
kanchan mahajan <kanchan.avi2803@gmail.com>
2024-10-26 13:08:06 +0530

    All Test cases passes

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String numbers) {
@@ -5,10 +8,61 @@ public class StringCalculator {
             return 0;
         }
 
-        //return  -1;  //placeholder for now
+        //return  -1;
 
-        return Integer.parseInt(numbers);
+       // return Integer.parseInt(numbers);
+
+//        String[] tokens = numbers.split(",");
+//        int sum = 0;
+//        for (String token : tokens) {
+//            sum += Integer.parseInt(token.trim());
+//        }
+//        return sum;
+
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+
+
+//        String[] tokens = numbers.split(delimiter);
+//        int sum = 0;
+//        for (String token : tokens) {
+//            sum += Integer.parseInt(token.trim());
+//        }
+//        return sum;
+
+        String[] numArray = numbers.split(delimiter);
+        int sum = 0;
+        List<Integer> negatives = new ArrayList<>();
+
+        for (String num : numArray) {
+            int number = Integer.parseInt(num.trim());
+            if (number < 0) {
+                negatives.add(number);
+            }
+            sum += number;
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negatives);
+        }
+
+        return sum;
+
 
     }
 
+//    public static void main(String args[]){
+//
+//        StringCalculator str = new StringCalculator();
+//        str.add("5,6");
+//
+//
+//    }
 }
+
+
+
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
 
 
@@ -16,9 +18,46 @@ public class StringCalculatorTest {
        assertEquals(5, calculator.add("5"));
     }
 
+    void testingCodeIsAddValue() {
+        StringCalculator calculator = new StringCalculator();
+        System.out.println("testing the single number return value 5");
+        assertEquals(5, calculator.add(",\n"));
+    }
 
 
+    void testTwoNumberReturnsSum() {
+       StringCalculator calculator = new StringCalculator();
+        System.out.println("testing two numbers returns the sum '3'");
+       assertEquals(3, calculator.add("1,2"));
+    }
+
+
+    void testNumberWithNewLineDelimiter() {
+       StringCalculator calculator = new StringCalculator();
+        System.out.println("test Number With NewLine Delimiter and perform add operation 1,2,3 =6");
+       assertEquals(6, calculator.add("1\n2,3"));
+     }
+
+    void testCustomDelimiter(){
+        StringCalculator calculator = new StringCalculator();
+        System.out.println("Testing the string is separated to ; 1;2;3");
+        assertEquals(3, calculator.add("//;\n1;2"));
+
+
+//        void testNegativeNumber(){
+//            StringCalculator calculator1 = new StringCalculator();
+//            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+//                calculator.add("1,-2,3,-4");
+//            });
+//            assertEquals("Negative numbers not allowed: [-2, -4]", exception.getMessage());
+//        }
+
+
+    }
 }
+
+
+
 
 
 
