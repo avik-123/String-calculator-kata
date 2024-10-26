188d14c13d86422d748afae1180651ce520bf894
kanchan mahajan <kanchan.avi2803@gmail.com>
2024-10-26 12:14:19 +0530

    new testcase pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class StringCalculator {
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        //return  -1;  //placeholder for now
+
+        return Integer.parseInt(numbers);
+
+    }
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class StringCalculatorTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class StringCalculatorTest {
+
+
+    @Test
+    void testAdd_EmptyString_ReturnsZero() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+
+
+    void testSingleNumberReturnsValue() {
+       StringCalculator calculator = new StringCalculator();
+       System.out.println("testing the single number return value 5");
+       assertEquals(5, calculator.add("5"));
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
